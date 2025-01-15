11edaf3678b41dc91f9444e18825753aaffeea38
dakshgoel2008 <goel.daksh2008@gmail.com>
2025-01-15 16:57:08 +0530

    SignUp and Login done

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { postSignup } from "../controller/user.js";
+import { postLogin, postSignup } from "../controller/user.js";
+import upload from "../utils/multer.js";
+
 const router = express.Router();
 
-router.post("/signup", postSignup);
+router.post("/signup", upload.single("image"), postSignup); // upload.single will run first....
 
-// router.post("/login", postLogin);
+router.post("/login", postLogin);
 
 export default router;
